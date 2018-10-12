5c68b54b4662d1cc4c878bb1d1e7a4c8e3ee024f
Teddy Milon <milonte@gmail.com>
2018-10-12 14:37:37 +0200

    S01_US15_Events_Page - Init

--- app/routing.php
+++ app/routing.php
@@ -9,12 +9,18 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
+
 $routes = [
     'Item' => [ // Controller
-        ['index', '/', 'GET'], // action, url, method
         ['add', '/item/add', ['GET', 'POST']], // action, url, method
         ['edit', '/item/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
         ['show', '/item/{id:\d+}', 'GET'], // action, url, method
         ['delete', '/item/delete/{id:\d+}', 'GET'], // action, url, method
     ],
+    'Home' => [ // Controller
+        ['index', '/', 'GET'], // action, url, method
+    ],
+    'Events' => [
+        ['index', '/events', 'GET'],
+    ]
 ];
