a788e825e437e31034c1f18f76cac2832b6b5788
Teddy Milon <milonte@gmail.com>
2018-10-12 11:13:54 +0200

    Changes structure of mvc View Folder (add Home/ & Events/)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace Controller;
+
+use Model\Home;
+use Model\HomeManager;
+
+/**
+ * Class HomeController
+ *
+ */
+class HomeController extends AbstractController
+{
+
+
+    /**
+     * Display Home listing
+     *
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function index()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+
+}
