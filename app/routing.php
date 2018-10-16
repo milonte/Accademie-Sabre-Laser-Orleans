775bde16cef8d74ab7286b85f07f0b55f5d31dce
Thomas Pecout <pecoutt@hotmail.fr>
2018-10-16 14:06:44 +0200

    retrait du 'POST' dans le routing 'Home'

--- app/routing.php
+++ app/routing.php
@@ -17,6 +17,6 @@ $routes = [
         ['delete', '/item/delete/{id:\d+}', 'GET'], // action, url, method
     ],
     'Home' => [ // Controller
-        ['index', '/', ['GET', 'POST']], // action, url, method
+        ['index', '/', ['GET']], // action, url, method
     ]
 ];
