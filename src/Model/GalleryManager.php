12e879b78495bb7a95063c79ad4d0b896b0d7c5d
Rémy DUCRY <ducry.remy@gmail.com>
2018-10-19 11:11:39 +0200

    Galerie images avec modal et responsive, faisant appel à la base de données images

---
+++ src/Model/GalleryManager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace Model;
+
+
+class GalleryManager extends AbstractManager
+{
+
+    const TABLE = 'images';
+
+    /**
+     * GalleryManager constructor.
+     * @param \PDO $pdo
+     */
+    public function __construct(\PDO $pdo)
+    {
+        parent::__construct(self::TABLE, $pdo);
+    }
+
+}
---
+++ src/Model/Images.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 19/10/18
+ * Time: 10:01
+ */
+
+namespace Model;
+
+/**
+ * Class Images
+ * @package Model
+ */
+class Images
+{
+
+}
