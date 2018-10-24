b74402152ea5e8a63759da91cc4b4cb0df60728c
Thomas Pecout <pecoutt@hotmail.fr>
2018-10-24 15:31:15 +0200

    modification affichage des erreurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -29,29 +29,29 @@ class HomeController extends AbstractController
     {
         $errorsForm = [];
         if (empty($userData['lastname'])) {
-            $errorsForm['lastname0'] = "Votre nom doit être indiqué";
+            $errorsForm['lastname'] = "Votre nom doit être indiqué";
         } elseif (!preg_match("#[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$#", $userData['lastname'])) {
-            $errorsForm['invalid lastname'] = "Votre nom ne doit pas contenir de caractères spéciaux";
+            $errorsForm['lastname'] = "Votre nom ne doit pas contenir de caractères spéciaux";
         }
         if (empty($userData['firstname'])) {
-            $errorsForm['firstname0'] = "Votre prénom doit être indiqué";
+            $errorsForm['firstname'] = "Votre prénom doit être indiqué";
         } elseif (!preg_match("#[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$#", $userData['firstname'])) {
-            $errorsForm['invalid firstname'] = "Votre prénom ne doit pas contenir de caractères spéciaux";
+            $errorsForm['firstname'] = "Votre prénom ne doit pas contenir de caractères spéciaux";
         }
         if (empty($userData['email'])) {
-            $errorsForm['email0'] = "Votre mail doit être indiqué";
+            $errorsForm['email'] = "Votre mail doit être indiqué";
         } elseif (!preg_match(" /^.+@.+\.[a-zA-Z]{2,}$/ ", $userData['email'])) {
-            $errorsForm['invalid email'] = "Le format de l'email n'est pas correct";
+            $errorsForm['email'] = "Le format de l'email n'est pas correct";
         }
         if (empty($userData['num'])) {
-            $errorsForm['num0'] = "Votre numéro de téléphone doit être indiqué";
+            $errorsForm['num'] = "Votre numéro de téléphone doit être indiqué";
         } elseif (!preg_match(" #^[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}?$# ", $userData['num'])) {
-            $errorsForm['invalid phone'] = "Le numéro de téléphone renseigné est incorrect";
+            $errorsForm['num'] = "Le numéro de téléphone renseigné est incorrect";
         }
         if (empty($userData['message'])) {
-            $errorsForm['message0'] = "Vous devez écrire un message";
+            $errorsForm['message'] = "Vous devez écrire un message";
         } elseif (!preg_match(" #[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$# ", $userData['message'])) {
-            $errorsForm['invalid message'] = "Votre message ne doit pas contenir de caractère non-autorisés";
+            $errorsForm['message'] = "Votre message ne doit pas contenir de caractère non-autorisés";
         }
 
 
