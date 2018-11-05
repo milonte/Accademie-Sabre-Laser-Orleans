3f7a9cd622bc48c8b30a7f56cebadb925b17a77b
Rémy DUCRY <ducry.remy@gmail.com>
2018-11-05 20:17:51 +0100

    Fin des corrections, conformité travis à 100%.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -29,14 +29,16 @@ class HomeController extends AbstractController
     private function verifMail(array $userData) : array
     {
         $errorsForm = [];
+        $carConformity = "#[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$#";
+        $numberConformity = " #^[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}?$# ";
         if (empty($userData['lastname'])) {
             $errorsForm['lastname'] = "Votre nom doit être indiqué";
-        } elseif (!preg_match("#[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$#", $userData['lastname'])) {
+        } elseif (!preg_match($carConformity, $userData['lastname'])) {
             $errorsForm['lastname'] = "Votre nom ne doit pas contenir de caractères spéciaux";
         }
         if (empty($userData['firstname'])) {
             $errorsForm['firstname'] = "Votre prénom doit être indiqué";
-        } elseif (!preg_match("#[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$#", $userData['firstname'])) {
+        } elseif (!preg_match($carConformity, $userData['firstname'])) {
             $errorsForm['firstname'] = "Votre prénom ne doit pas contenir de caractères spéciaux";
         }
         if (empty($userData['email'])) {
@@ -46,12 +48,12 @@ class HomeController extends AbstractController
         }
         if (empty($userData['num'])) {
             $errorsForm['num'] = "Votre numéro de téléphone doit être indiqué";
-        } elseif (!preg_match(" #^[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}[-/ ]?[0-9]{2}?$# ", $userData['num'])) {
+        } elseif (!preg_match($numberConformity, $userData['num'])) {
             $errorsForm['num'] = "Le numéro de téléphone renseigné est incorrect";
         }
         if (empty($userData['message'])) {
             $errorsForm['message'] = "Vous devez écrire un message";
-        } elseif (!preg_match(" #[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ' ]$# ", $userData['message'])) {
+        } elseif (!preg_match($carConformity, $userData['message'])) {
             $errorsForm['message'] = "Votre message ne doit pas contenir de caractère non-autorisés";
         }
 
