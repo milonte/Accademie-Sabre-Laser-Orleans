47d05e0f48366fd05be03ebd60df20b26b8346a7
milonte <milonte@gmaim.com>
2018-11-06 11:31:40 +0100

    fix js

--- public/assets/scripts/events.js
+++ public/assets/scripts/events.js
@@ -1,14 +1,13 @@
 function cutLongerText(textToTrim, maxLength) {
-    if (textToTrim.length>maxLength) {
         return textToTrim.substring(0, maxLength) + "[...]"
-    }
 }
 
 let textElt = document.getElementsByClassName("card-text");
 
 const MAX_LEGTH_TEXT = 500;
 
-
 for(let i=1; i<textElt.length; i++) {
+    if (textElt[i].innerHTML.length>MAX_LEGTH_TEXT) {
     textElt[i].innerHTML = cutLongerText(textElt[i].innerHTML, MAX_LEGTH_TEXT);
+    }
 }
